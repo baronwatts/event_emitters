4a3239e5ee7b326ccecdec1e7c6f0114dfd9c460
baron <watts.baron@hotmail.com>
2016-11-28 17:27:40 -0800

    first commit

---
+++ event_emitters.js
@@ -0,0 +1,68 @@
+/*The .apply() method automatically invokes or executes a function.
+The first parameter is the object that you want to invoke or execute
+The second parameter is the arguments that you want to pass into that function.
+
+*/
+
+
+/* ES5
+---------------------------------------------------------------*/
+function EventEmitter(){
+	this.events = {};
+}
+
+EventEmitter.prototype.on = function(type, listener){
+	this.events[type] = this.events[type] || []; //Assign an array for our type if one doesn't already exist
+	this.events[type].push(listener); //Then we will add listeners the type's array
+}
+
+EventEmitter.prototype.emit = function(type){
+	//checks to see if the type exist inside the this.events object then we will loop through that array that is assigned to that type and invoke all of the listeners.
+	if(this.events[type]){
+		this.events[type].forEach(function(listener){
+			listener();
+		});
+	}
+}
+
+var ee = new EventEmitter();
+ee.on('greet', function(){ console.log("Emitted!"); });
+
+
+ee.emit('greet'); //invokes all the functions that are assigned to the greet property inside the this.events object
+
+
+
+
+
+/* ES6
+---------------------------------------------------------------*/
+
+class EventEmitter {
+	constructor(){
+		this.events = {};
+	}
+
+	on(type, listener){
+		this.events[type] = this.events[type] || []; 
+		this.events[type].push(listener);
+	}
+
+	
+	emit(type){
+		if(this.events[type]){
+			this.events[type].forEach( (listener) => {
+				listener(); 
+			});
+		}
+	}
+}
+
+const ee = new EventEmitter();
+ee.on('greet', () => {
+	console.log("Emitted!");
+});
+
+
+
+ee.emit('greet');
